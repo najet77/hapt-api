ff557f9d6e9f37db086ab2b3fe7b40b383c3c79f
Malek Boubakri <malekbouba@gmail.com>
2019-04-27 22:04:15 +0100

    Remove ignored files

--- build/config/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var mongoURI = exports.mongoURI = "mongodb://localhost:27017/hapt";
-var port = exports.port = process.env.PORT || 3000;
-var JWTSECRET = exports.JWTSECRET = "secretjwt";
