e091fa493ad4030706a7e6fc7d7f0ac53eece43d
Malek Boubakri <malekbouba@gmail.com>
2019-05-13 02:15:38 +0100

    Lost state of user after a while of use #1

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,9 @@
 import { requireAdmin } from '../config/roles';
 import { requireAuth } from '../config/passport';
 import { create, findAll, findOne, update, remove, upload, saveFile } from '../controllers/userController.js';
-import { createNoPermission } from '../controllers/userController';
+import { createNoPermission, getAuthUser } from '../controllers/userController';
 
 export default function(router) {
-  // Create a new user without permissions!
-  router.post('/register', createNoPermission);
-
   // Create a new user
   router.post('/users', requireAuth, requireAdmin, create);
 
@@ -14,11 +11,17 @@ export default function(router) {
   router.get('/users', requireAuth, requireAdmin, findAll);
 
   // Retrieve a single User with userId
-  router.get('/users/:userId', requireAuth, requireAdmin, findOne);
+  router.get('/users/:userId', requireAuth, findOne);
 
   // Update a User with userId
   router.put('/users/:userId', requireAuth, requireAdmin, upload, saveFile, update);
 
   // Delete a User with userId
   router.delete('/users/:userId', requireAuth, requireAdmin, remove);
+
+  // Retrieve the current authenticated User
+  router.get('/currentuser', getAuthUser);
+
+  // Create a new user without permissions!
+  router.post('/register', createNoPermission);
 }
